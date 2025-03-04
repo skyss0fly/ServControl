0518c83f2791d5e38484a87304f8f8e279606e55
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2025-03-04 19:29:33 +1000

    Update Main.php

--- src/skyss0fly/ServControl/Main.php
+++ src/skyss0fly/ServControl/Main.php
@@ -3,15 +3,18 @@
 namespace skyss0fly\ServControl;
 
 use pocketmine\plugin\PluginBase;
+use skyss0fly\ServControl\GamemodeManager;
+use skyss0fly\ServControl\AuthCommand;
 
 class Main extends PluginBase {
 
     private GamemodeManager $gamemodeManager;
-
+private AuthCommand $authCommand;
+    
     public function onEnable(): void {
         $this->gamemodeManager = new GamemodeManager($this);
         $this->authCommand = new AuthCommand($this);
-        
+        $this->getServer()->getCommandMap()->register("auth", $this->authCommand);
         $this->getLogger()->info("ServControl plugin enabled!");
     }
 
