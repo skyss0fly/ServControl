ace6460b31d3b5927407c7d295c5b82fff218fb6
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2025-03-04 19:06:43 +1000

    Update GamemodeChecker.php

--- src/skyss0fly/ServControl/GamemodeChecker.php
+++ src/skyss0fly/ServControl/GamemodeChecker.php
@@ -1,14 +1,14 @@
 <?php
-
 namespace skyss0fly\ServControl;
 
-use skyss0fly\ServControl\GamemodeManager;
 use pocketmine\event\Listener;
+use pocketmine\player\Player;
 
 class GamemodeChecker implements Listener {
 
-  function getPlayersGamemode(string $player){
-return $player->getGamemode();
-    
-  }
+    public function getPlayersGamemode(Player $player): int {
+        return $player->getGamemode();
+    }
+
+
 }
