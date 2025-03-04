c1989e9181862b7f7fc228772939c6c0c2e5aff2
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2025-03-04 18:58:55 +1000

    Update GamemodeManager.php

--- src/skyss0fly/ServControl/GamemodeManager.php
+++ src/skyss0fly/ServControl/GamemodeManager.php
@@ -1,4 +1,3 @@
-<?php
 namespace skyss0fly\ServControl;
 
 use pocketmine\plugin\PluginBase;
@@ -7,22 +6,23 @@ use pocketmine\Server;
 
 class GamemodeManager {
 
-    private string $defaultGamemode = $this->getConfig()->get("DefaultGamemode");
+    private string $defaultGamemode;
 
-    public function __construct(string $defaultGamemode) {
-        $this->defaultGamemode = $defaultGamemode;
+    public function __construct(PluginBase $plugin) {
+        $this->defaultGamemode = $plugin->getConfig()->get("DefaultGamemode"); 
     }
 
     public function getDefaultGamemode(): string {
         return $this->defaultGamemode;
     }
 
- /*
- For Later Use
-  public function setDefaultGamemode(string $gamemode): void {
+    /*
+    For Later Use
+    public function setDefaultGamemode(string $gamemode): void {
         $this->defaultGamemode = $gamemode;
     }
-*/
+    */
+
     public function setPlayerToGamemode(Player $player, int $gamemode): bool {
         if (!$player->isOnline()) {
             return false;
@@ -31,4 +31,3 @@ class GamemodeManager {
         return true;
     }
 }
-?>
